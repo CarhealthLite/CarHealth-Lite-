a3beb965c6a70cd42805eec9d8fcb8f1e78cc9c9
CarhealthLite <kyanolaur07@yahoo.com>
2025-12-19 21:51:40 +0200

    Delete CarHealth-Lite directory

--- CarHealth-Lite/backend/routes/aiRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAISuggestion } = require('../controllers/aiController');
-
-router.post('/problem', getAISuggestion);
-
-module.exports = router;
--- CarHealth-Lite/backend/routes/vehicleRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getVehicles, addVehicle } = require('../controllers/vehicleController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.get('/', protect, getVehicles);
-router.post('/', protect, addVehicle);
-
-module.exports = router;
