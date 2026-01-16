0d4bea95c146c882f2d9ada64fb51444d77f98aa
CarhealthLite <kyanolaur07@yahoo.com>
2026-01-16 22:57:34 +0200

    api.js

--- CarHealth-Lite/client/src/services/api.js
+++ CarHealth-Lite/client/src/services/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
 
 const postRequest = async (endpoint, data) => {
   const response = await fetch(`${BASE_URL}${endpoint}`, {
@@ -11,13 +12,13 @@ const postRequest = async (endpoint, data) => {
 
 export const loginUser = async (email, password) => {
   const res = await postRequest('/auth/login', { email, password });
-  if (res.success && res.token) sessionStorage.setItem('token', res.token); // salveaza token-ul
+  if (res.success && res.token) sessionStorage.setItem('token', res.token); 
   return res;
 };
 
 export const signupUser = async (name, email, password) => {
   const res = await postRequest('/auth/signup', { name, email, password });
-  if (res.success && res.token) sessionStorage.setItem('token', res.token); // salveaza token-ul
+  if (res.success && res.token) sessionStorage.setItem('token', res.token); 
   return res;
 };
 
