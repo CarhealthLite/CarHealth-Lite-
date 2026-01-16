692e48281dab0879b8319b188ab6991355967e11
CarhealthLite <kyanolaur07@yahoo.com>
2026-01-16 21:25:46 +0200

    Signup.js

--- CarHealth-Lite/client/src/pages/Signup.js
+++ CarHealth-Lite/client/src/pages/Signup.js
@@ -1,4 +1,4 @@
-﻿import React, { useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { signupUser } from '../services/api';
 import { useAuth } from '../context/AuthContext';
